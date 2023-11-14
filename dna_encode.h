5e05d5bf7ce6bbffe1d621ebd8ac414762cf928c
Qing Chang <46983747+changQing111@users.noreply.github.com>
2023-11-14 10:31:22 +0800

    Update dna_encode.h

--- dna_encode.h
+++ dna_encode.h
@@ -2,27 +2,18 @@
 #include <string.h>
 #include <stdint.h>
 
-int8_t base_to_bin(char base) {
-    switch (base) {
-    case 'A': 
-        return 0;
-    case 'T': 
-        return 1;
-    case 'C': 
-        return 2;
-    case 'G': 
-        return 3;
-    }
-}
+int base_map[128] = {[0 ... 127] = -1};
+base_map['A'] = 0;
+base_map['C'] = 1;
+base_map['G'] = 2;
+base_map['T'] = 3;
 
-uint32_t seq_to_bin(char* seq) {
-    uint32_t binary_kmer = 0;
-    int n = strlen(seq);
-    for(int i = 0; i < n; i++) {
-        int8_t val = base_to_bin(*(seq + i));
-        binary_kmer <<= 2;
-        binary_kmer |= val;
+int seq_to_num(char* seq, int* BaseMap) {
+    int length = strlen(seq);
+    int num = 0;
+    for(int i = 0; i < length; i++) {
+        int left_move = 2*(length-1-i);
+        num |= (BaseMap[seq[i]] << left_move);
     }
- 
-    return binary_kmer;
+    return num;
 }
