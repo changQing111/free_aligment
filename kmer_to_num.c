842f15020c781e0e359942cb376734e22b09fc63
Qing Chang <46983747+changQing111@users.noreply.github.com>
2023-11-14 14:48:18 +0800

    Create kmer_to_num.c

---
+++ kmer_to_num.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#define KMER 4
+
+int* kmer_to_num(char* seq, int length, int* num_array, int* BaseMap) {
+    int set_zore = 0;
+    for(int i = 0; i < 2*KMER-2; i++)  {
+        set_zore |= 1<<i;
+    }
+    //printf("%d\n", set_zore);
+    int num = 0;
+    for(int i = 0; i < KMER; i++) {
+        int left_move = 2*(KMER-1-i);
+        num |= (BaseMap[seq[i]] << left_move);
+    }
+    num_array[0] = num;
+    for(int j = KMER; j < length; j++) {
+        num = ((num & set_zore) << 2) | BaseMap[seq[j]];
+        num_array[j-KMER+1] = num;
+    }
+
+    return num_array;
+}
+
+int main() {
+    int base_map[128] = {[0 ... 127] = -1};
+    base_map['A'] = 0;
+    base_map['C'] = 1;
+    base_map['G'] = 2;
+    base_map['T'] = 3;
+
+    char* seq = "CTAGCTGCTAATGTCTAGCTCGTA";
+    int len = strlen(seq);
+    //printf("seq length = %d\n", len);
+    int kmer_num = len - KMER + 1;
+    int num_arr[kmer_num];
+    int* res = kmer_to_num(seq, len, num_arr, base_map);
+
+    for(int i = 0; i < kmer_num; i++) {
+        printf("%d\n", res[i]);
+    }
+
+    return 0;
+}
