5c1cea2503bcd657ef145f056c7b89c9e9f0f6bb
Qing Chang <46983747+changQing111@users.noreply.github.com>
2023-11-14 19:12:32 +0800

    Update dna_encode.h

--- dna_encode.h
+++ dna_encode.h
@@ -6,13 +6,3 @@ base_map['A'] = 0;
 base_map['C'] = 1;
 base_map['G'] = 2;
 base_map['T'] = 3;
-
-int seq_to_num(char* seq, int* BaseMap) {
-    int length = strlen(seq);
-    int num = 0;
-    for(int i = 0; i < length; i++) {
-        int left_move = 2*(length-1-i);
-        num |= (BaseMap[seq[i]] << left_move);
-    }
-    return num;
-}
