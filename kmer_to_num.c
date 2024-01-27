b57dcae628e2d152fbcb358c4c7953b44f47c4b7
Qing Chang <46983747+changQing111@users.noreply.github.com>
2024-01-28 00:28:58 +0800

    Update kmer_to_num.c

--- kmer_to_num.c
+++ kmer_to_num.c
@@ -1,23 +1,41 @@
 #include <stdio.h>
 #include <string.h>
 #include "dna_encode.h"
-#define KMER 4
+#define KMER 6
 
-int* kmer_to_num(char* seq, int length, int* num_array, int* BaseMap) {
-    int set_zore = 0;
-    for(int i = 0; i < 2*KMER-2; i++)  {
-        set_zore |= 1<<i;
+int* kmer_to_num(char* seq, int length, int* num_array, int* base_map, int* recv_comp) {
+    unsigned int set_zore = 0b01;
+    for(int i = 0; i < 2*(KMER-1)-1; i++)  {
+        set_zore = (set_zore << 1) | 0b01;
     }
-    int num = 0;
+    unsigned left_set_zore = set_zore;
+    //printf("left=%u\n", left_set_zore);
+    unsigned right_set_zore = set_zore << 2;
+    //printf("right=%u\n", right_set_zore);
+
+    unsigned int num = 0; 
+    unsigned int recv_comp_num = 0;
     for(int i = 0; i < KMER; i++) {
         //int left_move = 2*(KMER-1-i);
-        //num |= (BaseMap[seq[i]] << left_move);
-        num = (num << 2)|BaseMap[seq[i]];
+        //num |= (base_map[seq[i]] << left_move);
+        num = (num << 2)|base_map[seq[i]];
+        //recv_comp_num = (recv_comp_num >> 2) | (recv_comp[seq[i]] >> (KMER+1));
     }
-    num_array[0] = num;
+    //printf("first kmer:%d\n", num);
+    recv_comp_num = recv_comp[seq[0]];
+    for(int i = 1; i < KMER; i++) {
+        recv_comp_num |= (recv_comp[seq[i]] << (2*i));
+    }
+    //printf("first recv_comp kmer:%d\n", recv_comp_num);
+    
+    num_array[0] = (num < recv_comp_num) ? num : recv_comp_num;
+
     for(int j = KMER; j < length; j++) {
-        num = ((num & set_zore) << 2) | BaseMap[seq[j]];
-        num_array[j-KMER+1] = num;
+        num = ((num & left_set_zore) << 2) | base_map[seq[j]];
+        recv_comp_num = ((recv_comp_num & right_set_zore) >> 2) | (recv_comp[seq[j]] << (2*KMER-2));
+        //printf("normal kmer=%d\n", num);
+        //printf("recv kmer=%d\n", recv_comp_num);
+        num_array[j-KMER+1] = (num < recv_comp_num) ? num : recv_comp_num;
     }
 
     return num_array;
@@ -25,11 +43,13 @@ int* kmer_to_num(char* seq, int length, int* num_array, int* BaseMap) {
 
 int main() {
     base_map_init(base_map);
+    recv_comp_init(recv_comp);
+
     char* seq = "CTAGCTGCTAATGTCTAGCTCGTA";
     int len = strlen(seq);
     int kmer_num = len - KMER + 1;
     int num_arr[kmer_num];
-    int* res = kmer_to_num(seq, len, num_arr, base_map);
+    int* res = kmer_to_num(seq, len, num_arr, base_map, recv_comp);
 
     for(int i = 0; i < kmer_num; i++) {
         printf("%d\n", res[i]);
